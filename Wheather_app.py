c387a4533ee34eb700b88e76f24e9c2cc35d80a3
Adrian <adriangomis34@gmail.com>
2024-07-05 13:37:22 +0200

    Update Wheather_app.py

--- Wheather_app.py
+++ Wheather_app.py
@@ -1,5 +1,49 @@
 import tkinter as tk
 import requests as req
+from tkinter import Label, Entry
+from PIL import Image, ImageTk
+import ttkbootstrap as ttkb
+from Apiconfig import apikey
+
+
+root = ttkb.Window(themename="night")
+root.title("Weather app")
+root.geometry("400x400")
+
+def fetch_weather_icon(icon_url):
+    """
+    Function to get and create the icon for the app
+
+    return: None
+    """
+    try:
+        image = Image.open(req.get(icon_url, stream=True).raw)
+        return ImageTk.PhotoImage(image)
+    except Image.UnidentifiedImageError as error:
+        print(f"Error fetching weather icon: {error}")
+        return None
+
+def update_ui(icon, temperature, description, city, country):
+    """Function to configure and update the user interface when needed"""
+    location_label.configure(text=f"{city}, {country}")
+
+    if icon:
+        icon_label.configure(image=icon)
+        icon_label.image = icon 
+    
+    temperature_lbl.configure(text=f"Temperature: {temperature:.2f}ºC")
+    description_label.configure(text=f"Description: {description}")
+
+def search():
+    """Function to search weather for a city using a get method"""
+    city = city_entry.get()
+    result = get_weather(city)
+    if result is None:
+        return 
+    
+    icon_url, temperature, description, city, country = result
+    icon = fetch_weather_icon(icon_url)
+    update_ui(icon, temperature, description, city, country)
 
     
 # ENTRY WIDGET TO ENTER THE CITY NAME
