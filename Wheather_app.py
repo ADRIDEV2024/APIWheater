8c4c80032ce4e775f81299271ff929e6cb53d2af
Adrian <adriangomis34@gmail.com>
2024-07-07 10:21:43 +0200

    Update Wheather_app.py

--- Wheather_app.py
+++ Wheather_app.py
@@ -57,9 +57,9 @@ city_entry.pack(pady=10)
 search_button = ttkb.Button(root, text="Search", command=search, bootstyle="warning")
 search_button.pack(pady=10)
 
-# LABEL WIDGET TO SHOW THE LOCATION NAME 
 
-location_label = tk.Label(root, font="Cambria, 19")
+
+location_label = tk.Label(root, font="Cambria, 19") # LABEL WIDGET TO SHOW THE LOCATION NAME 
 location_label.pack(pady=18)
 
 
